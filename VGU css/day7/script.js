c6bee4caef8ea0ffc119288b20f940874da1994f
Ishant Yadav <ishant3010y@gmail.com>
2026-07-07 12:54:22 +0530

    js updates

--- VGU css/day7/script.js
+++ VGU css/day7/script.js
@@ -4,12 +4,16 @@ const container = document.querySelector("#container");
  container.style.display="flex";
  container.style.alignItems="center";
  container.style.justifyContent="center";
- container.style.direction="row";
- container.style.gap="20px";
+ container.style.flexDirection = "column";
+ container.style.gap="0px";
 
 
 const button = document.querySelector("#button");
 
-button.addEventListener("mouseenter", () => {
+button.addEventListener("mouseenter", (e) => {
     button.style.border = "2px solid red";
+    button.style.backgroundColor="lightgreen";
+    button.style.color="blue";
+    button.style.transform ="translateX(30px)";
+    button.style.transform ="translateY(30px)";
 });
---
+++ VGU css/day8/script.js
@@ -0,0 +1,37 @@
+const container= document.querySelector(".container");
+// container.style.display="flex";
+// container.style.justifyContent="center";
+container.style.height="500px";
+container.style.width="700px";
+container.style.marginTop="50px";
+container.style.marginLeft="200px";
+container.style.border="5px solid red";
+
+const fields= document.querySelector("#fields");
+fields.style.display="flex";
+fields.style.justifyContent="center";
+
+
+const taskInput =document.querySelector("#taskInput");
+taskInput.style.height="30px";
+taskInput.style.width="300px";
+taskInput.style.marginTop="20px";
+taskInput.style.marginLeft="30px";
+taskInput.style.border="2px solid blue";
+
+
+const button=document.querySelector("#button");
+button.style.height="30px";
+button.style.width="100px";
+button.style.marginTop="20px";
+button.style.marginLeft="20px";
+button.style.border="2px solid blue";
+button.style.backgroundColor="black";
+button.style.color="white";
+button.style.cursor="pointer";
+// button:hover.style.backgroundColor="green";
+
+
+
+const taskList=document.querySelector("#taskList");
+taskList.s
