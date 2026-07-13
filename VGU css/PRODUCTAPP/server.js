33567707889c39b9c69b14e69d67a4df586ecd2e
Ishant Yadav <ishant3010y@gmail.com>
2026-07-13 21:53:43 +0530

    CRUD

--- VGU css/PRODUCTAPP/server.js
+++ VGU css/PRODUCTAPP/server.js
@@ -11,7 +11,29 @@ app.get("/", (req, res)=> {
 
 app.get("/products", (req, res)=>{
     res.render("product",{products})
-})
+});
+
+app.get("/product/new",(req,res)=>{
+    res.render("new")
+});
+
+app.post("/products", (req, res)=>{
+     const {name,price,image,desc} =req.body;
+     let id;
+     if(products.length>0){
+        id=products[products.length-1].id+1;
+     }else{
+        id=1;
+     }
+     products.push({id,name,image,price,desc});
+     res.redirect("/products")
+});
+
+app.get("/products/:id", (req,res)=>{
+    const {id} =req.params;
+    const product =products.find((item)=> item.id==id);
+    res.render("show", {product})
+});
 
 const PORT =5000;
 app.listen(PORT, () => {
