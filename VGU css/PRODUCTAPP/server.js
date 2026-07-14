8223a7307bed81e3207cf30bdb965717aab3a09a
Ishant Yadav <ishant3010y@gmail.com>
2026-07-14 11:01:35 +0530

    Method Override

--- VGU css/PRODUCTAPP/server.js
+++ VGU css/PRODUCTAPP/server.js
@@ -1,40 +1,43 @@
 const express = require("express");
-const app =express();
-const products =require("./data/products");
+const app = express();
+const products = require("./data/products");
 
 app.set("view engine","ejs");
 app.use(express.urlencoded({extended:true}));
 
-app.get("/", (req, res)=> {
-    res.render("Home");
-});
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
 
-app.get("/products", (req, res)=>{
+app.get("/",(req,res)=>{
+    res.render("home");
+})
+
+app.get("/products",(req,res)=>{
     res.render("product",{products})
-});
+})
 
 app.get("/product/new",(req,res)=>{
     res.render("new")
-});
+})
 
-app.post("/products", (req, res)=>{
-     const {name,price,image,desc} =req.body;
-     let id;
-     if(products.length>0){
-        id=products[products.length-1].id+1;
-     }else{
+app.post("/products",(req,res)=>{
+    const {name,image,price,desc} = req.body;
+    let id;
+    if(products.length>0){
+        id=products[products.length-1].id+1
+    }else{
         id=1;
-     }
-     products.push({id,name,image,price,desc});
-     res.redirect("/products")
-});
+    }
+    products.push({id,name,image,price,desc});
+    res.redirect("/products")
+})
 
-app.get("/products/:id", (req,res)=>{
-    const {id} =req.params;
-    const product =products.find((item)=> item.id==id);
-    res.render("show", {product})
-});
+app.get("/products/:id",(req,res)=>{
+    const {id} = req.params;
+    const product = products.find((item)=> item.id==id);
 
+    res.render("show",{product})
+})
 
 app.get("/products/:id/edit",(req,res)=>{
     const {id} = req.params;
@@ -63,9 +66,7 @@ app.delete("/products/:id",(req,res)=>{
     res.redirect("/products")
 })
 
-
-const PORT =5000;
-app.listen(PORT, () => {
-     console.log("Server run at port",PORT);
-
+const PORT = 5000;
+app.listen(PORT,()=>{
+    console.log("Server run at port",PORT);
 })
