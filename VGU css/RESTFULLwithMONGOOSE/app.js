81da7ba002c6d573e3f14ed1e94eb6d1e62ef33b
Ishant Yadav <ishant3010y@gmail.com>
2026-07-16 11:10:40 +0530

    ConnectDBwithExpress

--- VGU css/RESTFULLwithMONGOOSE/app.js
+++ VGU css/RESTFULLwithMONGOOSE/app.js
@@ -1,5 +1,20 @@
 const express = require("express");
 const app = express();
+const mongoose =require("mongoose");
+const Products = require("./models/product");
+
+mongoose.connect("mongodb://localhost:27017/VGU-Ecom")
+    .then(()=>{
+        console.log("DB Connected");
+    })
+    .catch(()=>{
+        console.log("DB not connected");
+    })
+
+// yaha database ka schema and model banate hai but yaha banane se app.js ka code messy 
+// ho jata hai esliye schema and model ham "models" folder me banayege and uske baad 
+// yaha require kar lenge
+    
 
 app.set("view engine","ejs");
 
