8526955789d2521f27c4091d0a393b393b470ce1
Ishant Yadav <ishant3010y@gmail.com>
2026-07-16 23:26:59 +0530

    FilesInViews

--- VGU css/RESTFULLwithMONGOOSE/app.js
+++ VGU css/RESTFULLwithMONGOOSE/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const mongoose =require("mongoose");
 const Products = require("./models/product");
+const path = require("path");
+
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended:true}));
 
 mongoose.connect("mongodb://localhost:27017/VGU-Ecom")
     .then(()=>{
@@ -20,6 +24,51 @@ app.set("view engine","ejs");
 
 app.get("/",(req,res)=>{
     res.render("home");
+})
+
+app.get("/products",async (req, res)=>{
+    const products = await Products.find({});
+    // console.log(products)
+    res.render("index",{products});
+})
+
+app.get("/product/new",(req, res)=>{
+    res.render("new");
+})
+
+app.post("/products",async (req,res)=>{
+    const {name,image,price,desc} = req.body;
+    await Products.create({name,image,price,desc});
+    res.redirect("/products")
+})
+
+
+app.get("/products/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Products.findById(id);
+        res.render("show", { product });
+    } catch (error) {
+        console.log(error);
+        res.status(404).send("Product not found");
+    }
+});
+
+app.get("/product/:id/edit", async(req, res)=>{
+    res.render("edit", {product})
+})
+
+app.put("/product/:id", async(req, res)=>{
+    const { id } = req.params;
+    const{name, image, price, desc} =req.body;
+    const product = await Products.findByIdAndUpdate(id);
+    res.render("/product")
+})
+
+app.delete("/product/:id", async(req, res)=>{
+    const {id} = req.params;
+    await Products.findByIdAndDelete(id)
+    res.redirect("/products")
 })
 
 const PORT = 4000;
